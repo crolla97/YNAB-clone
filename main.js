11f4c7f861cff1887f2da02cbefd77c2554e1b21
crolla97 <cristianocrolla@gmail.com>
2019-02-28 12:46:08 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+//* Budget Controller
+let budgetController = (function() {
+
+})();
+
+//* UI Controller
+let UIController = (function() {
+
+})();
+
+//* Global Controller
+let controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.budget-toolbar-add-category').addEventListener('click', function() {
+        
+        // Toggle overlay display
+        let overlay = document.getElementById('overlay');
+        overlay.style.display = 'block';
+
+        document.onclick = function(e) {
+            if (e.target.id == 'overlay') {
+                overlay.style.display = 'none';
+            } else if (e.target.id == 'cancel') {
+                overlay.style.display = 'none';
+            } else if (e.target.id == 'primary') {
+                overlay.style.display = 'none';
+            }
+        };
+    });
+
+})(budgetController, UIController);
