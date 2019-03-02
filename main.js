1d5fb48f9131592f55d4b9f740f6a5fa1b87e8b8
crolla97 <cristianocrolla@gmail.com>
2019-03-02 15:22:14 +0000

    Add Category Button
    When a button is pressed with class 'add-category', the add category popup appears relative to where the button was pressed on the screen.This is important for when there will be multiple master categories in need of sub categories

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 //* Budget Controller
-let budgetController = (function() {
+let budgetController = (function () {
 
 })();
 
 //* UI Controller
-let UIController = (function() {
+let UIController = (function () {
 
 })();
 
 //* Global Controller
-let controller = (function(budgetCtrl, UICtrl) {
+let controller = (function (budgetCtrl, UICtrl) {
+    // Toggle overlay display
+    let addCat;
 
-    document.querySelector('.add-category').addEventListener('click', function() {
-        
-        // Toggle overlay display
-        let overlay = document.getElementById('overlay');
-        overlay.style.display = 'block';
+    addCat = document.querySelectorAll('.add-category');
 
-        document.onclick = function(e) {
-            if (e.target.id == 'overlay') {
-                overlay.style.display = 'none';
-            } else if (e.target.id == 'cancel') {
-                overlay.style.display = 'none';
-            } else if (e.target.id == 'primary') {
-                overlay.style.display = 'none';
-            }
-        };
+    addCat.forEach(function (button) {
+        button.addEventListener('click', function(event) {
+            let x, y, overlay, popup;
+
+            x = event.clientX;
+            y = event.clientY;
+
+            console.log(x, y);
+
+            overlay = document.getElementById('overlay');
+            popup = document.querySelector('.model');
+            overlay.style.display = 'block';
+            popup.style.top = `${y + 25}px`;
+            popup.style.left = `${x - 115}px`;
+
+            document.onclick = function (e) {
+                if (e.target.id == 'overlay') {
+                    overlay.style.display = 'none';
+                } else if (e.target.id == 'cancel') {
+                    overlay.style.display = 'none';
+                } else if (e.target.id == 'primary') {
+                    overlay.style.display = 'none';
+                }
+            };
+        })
     });
 
 })(budgetController, UIController);
